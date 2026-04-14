a0fca176fd4645c5bf958d139296b01e4d633f47
Yedinson57 <yedinson960@gmail.com>
2026-04-14 09:30:24 -0500

    modified

--- hola-mundo/src/components/Main.jsx
+++ hola-mundo/src/components/Main.jsx
@@ -3,7 +3,7 @@ function Main() {
   return (
     <main className="main">
       <h2 id="#1">Contenido Principal</h2>
-      <p>Bienvenido a mi aplicación React</p>
+      <p>Bienvenido a mi aplicación React:</p>
       <img src="https://images7.memedroid.com/images/UPLOADED929/60e45c870f5ed.jpeg" alt="fino" title="finoS"/>
     </main>
   );
